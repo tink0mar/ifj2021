a198798ee0fb16c57b27ac178f4632f77857861c
tink0mar <kozak.martin2000@gmail.com>
2021-11-24 21:22:59 +0100

    ADD: added new files

---
+++ error.c
@@ -0,0 +1,11 @@
+/**
+ * @file error.c
+ * @brief 
+ * @author 
+ * 
+ * Project: IFJ compiler
+ * Date: 24.11.2021
+ */ 
+
+
+
---
+++ error.h
@@ -0,0 +1,8 @@
+/**
+ * @file error.h
+ * @brief definition of errors
+ * @author 
+ * 
+ * Project: IFJ compiler
+ * Date: 24.11.2021
+ */ 
