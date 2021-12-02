3b81d71e36a6671e950e625e676319c322f2cf72
tink0mar <kozak.martin2000@gmail.com>
2021-12-02 18:59:39 +0100

    EDIT: added global dec, def and call

--- parser.c
+++ parser.c
@@ -7,155 +7,316 @@
  * Date: 24.11.2021
  */ 
 
-
-#include "scanner.h"
 #include <stdlib.h>
 #include <stdbool.h>
 #include "error.h"
 #include "parser.h"
+#include "scanner.h"
+#include "symtable.h"
+#include "enum_list.h"
 
+bool check_tokens(TokenType token_type){
+    TokenType arr[5] = {TT_IDENTIFIER, TT_INTEGER, TT_STRING, TT_NUMBER, TT_KW_NIL};
 
-TToken *get_token_stack(){
+    for (int i = 0; i < 5; i++){
+        if (token_type == arr[i])
+            return true;
+    }
+    return false;
 
+}
+
+void free_resources(ParserData *p_data){
+
+    //token
+    free(p_data->token->attribs.string);    
+    free(p_data->token);
+
+    //global frame dispose
+    bst_dispose( &(p_data->global_frame) );
+
+    //stack dispose
+    sym_stack_dispose(&(p_data->stack));
+    free(p_data);
+
+}
+
+ParserData *parser_init(){
+
+    ParserData *p_data;
+
+    p_data = malloc(sizeof(ParserData));
+
+    if (p_data == NULL){
+        return NULL;
+    }
+
+    p_data->token = malloc(sizeof(Token));
+
+    if (p_data->token == NULL) {
+        free(p_data);
+        return NULL;
+    }
+
+    p_data->token->attribs.string = NULL;
+    bst_init(&(p_data->global_frame));
+
+    return p_data;
 }
 
 
 
-bool return_types_list(){
+bool data_types_list(ParserData *p_data, char *id){
 
-    TToken *token = get_token_stack();
+    Token *token = p_data->token;
 
-    if (token->type == TT_EOF || token->type == TT_KW_GLOBAL || token->type == TT_KW_FUNCTION) {
+    GET_TOKEN(token);
+
+    switch(token->type){
+
+        case TT_RIGHT_PAR:
+            void bst_insert_fun(&(p_data->global_frame), id, 0, NULL, 0, NULL);
+            return true;
+            break;
+
+        case TT_STRING:
+
+            break;
+
+        case TT_NUMBER:
+
+            break;
+
+        case TT_INTEGER:
+
+            break;
+        
+        case TT_KW_NIL:
+
+            break;
+        
+        default:
+            set_error(SYNTACTIC_ERR);
+            return false;
+    }
 
 
-        return true; 
 
-    } else if () {
+}
+
+
+/**
+ * <body> -> GLOBAL ID : FUNCTION ( <data_types_list> ) <return_types_list> <body>
+ */
+bool global(ParserData *p_data) {
+    Token *token =  p_data->token;
+
+    GET_TOKEN(token);
+
+    // check token for identifier
+    if (token->type == TT_IDENTIFIER){
+        
+        if (bst_search(p_data->global_frame, token->attribs.string) != NULL){
+            set_error(SEM_UNDEFINED_ERR);
+            return false;
+        }
+
+        // save function ID for later
+        char *fun_id = token->attribs.string; 
+
+        // check colon
+        GET_TOKEN(token);
+        CHECK_VARS(token->type,TT_COLON, SYNTACTIC_ERR);
+        
+        //check "function"
+        GET_TOKEN(token);
+        CHECK_VARS(token->type,TT_KW_FUNCTION, SYNTACTIC_ERR);
+        
+        // check left parenthise
+        GET_TOKEN(token);
+        CHECK_VARS(token->type,TT_LEFT_PAR, SYNTACTIC_ERR);
+
+        if ( (data_types_list(p_data, fun_id)) ) {
+            return true;
+        } else {
+            return false;
+        }
+
+        // TODO return types
 
     }
 
 }
 
-bool data_types_list(){
+bool function_param_list_others(ParserData *p_data, bool is_global_call) {
 
-    TToken *token = get_token();
+    Token *token = p_data->token;
+    DataType *params = NULL;
+    //Bad parameters
+    
 
-    if (token->type == TT_RIGHT_PAR){
+
+
+    return true;
+
+}
+
+
+/**
+ *
+ * <function_param_list> -> <function_param> <function_param_list_others>
+ * <function_param_list_others> -> eps
+ * <function_param_list_others> -> , <function_param> <function_param_list_others
+ */
+
+bool function_param_list(ParserData *p_data, bool is_global_call, TreeNode *func){
+
+    //MAM TOKEN
+    Token *token = p_data->token;
+
+    //Bad parameters
+    
+}
+
+
+/**
+ * <function_call> -> ID ( <function_param_list> )
+ */
+bool function_call(ParserData *p_data, bool is_global_call){
+
+    Token *token = p_data->token;
+
+    // check if ID of function is in global frame
+
+    TreeNode *func = bst_search(p_data->global_frame, token->attribs.string);
+
+    // check if ID of function is in global frame
+    if ( func == NULL ){
+        set_error(SEM_UNDEFINED_ERR);
+        return false;
+    }
+
+    // check if function was defined
+    CHECK_VARS(func->fun_extension->is_defined,true, SEM_UNDEFINED_ERR);
+
+    // check if function has return types
+    // TODO NEVIEM AKA CHYBA
+    if (is_global_call) {
+        CHECK_VARS(func->fun_extension->cnt_return_type, 0, );
+    }
+
+    // check left parenthise
+    GET_TOKEN(token);
+    CHECK_VARS(token->type, TT_LEFT_PAR, SYNTACTIC_ERR);
+
+    GET_TOKEN(token);
+
+    // <function_param_list> -> eps
+    if (token->type == TT_RIGHT_PAR) {
+        
+        // TODO CALL EMPTY function
+
         
         return true;
-    
+    } else if ( function_param_list(p_data, is_global_call, func) ) {
+
+        return true;
+
+    } else {
+        return false;
+    }
+
+}
+
+/**
+ * <body> -> eps
+ * <body> -> GLOBAL ID : FUNCTION ( <data_types_list> ) <return_types_list> <body>
+ * <body> -> FUNCTION ID ( <param_list> ) <return_types_list> <block> END <body>
+ * <body> -> <function_call> <body>
+ */
+
+bool body(ParserData *p_data){
+    Token *token = p_data->token;
+
+    GET_TOKEN(token);
+
+    // eps
+    if (token->type == TT_EOF){
+        return true;
     } else {
 
-        switch(token->type) {
-
-            case TT_STRING:
-
+        switch(token->type){
+            
+            // global
+            case TT_KW_GLOBAL:
+                return global(p_data) && body(p_data);
                 break;
-            case TT_NUMBER:
-
+            
+            // function
+            case TT_KW_FUNCTION:
+                return body(p_data);
                 break;
-            case TT_INTEGER:
-
+            
+            //call function
+            case TT_IDENTIFIER:
+                return function_call(p_data, true) && body(p_data);
                 break;
-            case TT_KW_NIL:
-
-                break;
-            default:
-                return false;
-
         }
-
     }
 
-    return false;
-    
-}
-
-
-bool body(){
-
-    bool body = false;
-
-    TToken *token = get_token();
-
-    if (token->type == TT_EOF) {
-
-        return true;
-
-    // GLOBAL ID : FUNCTION
-    } else if (token->type != TT_KW_GLOBAL) {
-
-
-        token = get_token();
-            
-        if (token->type != TT_IDENTIFIER){
-            
-            // IMPORTANT
-
-        }
-
-        token = get_token();
-
-        if (token->type != TT_COLON){
-
-        }
-
-        token = get_token();
-
-        if (token->type != TT_KW_FUNCTION) {
-
-        }
-
-        token = get_token();
-
-        if (token->type != TT_LEFT_PAR) {
-                
-        }
-
-        if ( data_types_list() == false) {
-
-            return false;
-        }
-
-        if ( return_types_list() == false) {
-
-            return false;
-        }
-
-
-
-    // FUNCTION ID ()
-    } else if (token->type == TT_KW_FUNCTION) {
-
-        
-
-    } else if (token->type == TT_IDENTIFIER) {
-
-    }
-
+    set_error(SYNTACTIC_ERR);
     return false;
 
 }
 
-bool program(){
 
-    TToken *token  = get_token();
-    
+/**
+ * <program> -> REQUIRE STRING <body> 
+*/
+bool program(ParserData *p_data){
+    Token *token = p_data->token;
 
+    GET_TOKEN(token);
+
+    // first word has to be "require"
     if ( token->type == TT_KW_REQUIRE) {
-        token = get_token();
+
+        GET_TOKEN(token);
+
+        // "ifj 21" after require
         if (token->type == TT_STRING && strcmp(token->attribs.string, "ifj 21")){
-            return true && body();
+            return true && body(p_data);
         }
+
     }
+     
+    //no require or string
+    set_error(SYNTACTIC_ERR);
     return false;
+
 }
 
-void syntactic_analyzator(){
 
-    if ( program() == true ) {
+//main function for parser
+int syntactic_analyzator(){
 
-    } 
+    ParserData *p_data;
+
+    p_data = parser_init();
+
+    if (p_data == NULL) {
+        error_mess(INTERNAL_ERR);
+        return INTERNAL_ERR;
+    }
+
+    if ( program(p_data)){
+        free_resources(p_data);
+        return 0;
+    } else {
+        error_mess(num_error);
+        free_resources(p_data);
+        return num_error;
+    }
 
 }
 
--- parser.h
+++ parser.h
@@ -8,10 +8,69 @@
  */ 
 
 #include <stdbool.h>
+#include "symtable.h"
+#include "scanner.h"
+#include "error.h"
+#include "enum_list.h"
+
+#define GET_TOKEN(TOKEN) \
+    get_token(TOKEN); \
+    if (num_error != 0) { \
+    return false;\
+    }\
+
+#define CHECK_TOKEN(TOKEN_TYPE, TYPE, ERR) \
+    if (TOKEN_TYPE != TYPE) { \
+        set_error(ERR); \
+        return false; \
+    } \
+    
+#define CHECK_VARS(A,B,ERR)\
+    if (A != B) { \
+        set_error(ERR); \
+        return false; \
+    }\
 
 
-bool ();
+typedef struct {
+
+    Token *token;
+    SymStack stack;
+    TreeNode *global_frame;
+
+} ParserData;
 
 
 bool program();
+
+/**
+ * @brief Main program for parser
+ * 
+ * @return int 
+ */
+int syntactic_analyzator();
+
+/**
+ * @brief Function frees all allocated resources
+ * 
+ */
+void free_resources(ParserData *p_data);
+
+/**
+ * @brief Function allocates data for parser
+ * 
+ * @return ParserData * 
+ */
+ParserData *parser_init();
+
+/**
+ * @brief Function for checking tokens
+ * 
+ * @param token_type Type of token 
+ * @param arr Array of token types
+ * @param len Length of arr
+ * @return true  
+ * @return false 
+ */
+bool check_tokens(TokenType token_type);
 
