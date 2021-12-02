9e58a4e2485bfe97739606cae6f8ba0435498928
tink0mar <kozak.martin2000@gmail.com>
2021-12-02 13:26:24 +0100

    ADD: Interface for enum lists

---
+++ enum_list.c
@@ -0,0 +1,46 @@
+/**
+ * @file enum_list.c
+ * @brief Functions for dynamic enumerate array
+ * @author Denis Adásek
+ *
+ * Project: IFJ compiler
+ * Date: 24.11.2021
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include "enum_list.h"
+
+bool enum_append(DataType **list, DataType type, int *len){
+
+    DataType *ptr = (DataType *)realloc(*list, sizeof(DataType) * (++*len)); 
+    
+    if (ptr == NULL){
+        return false;
+    } else {
+        
+        ptr[0] = type;
+        *list = ptr;
+        return true;
+    }
+}
+
+void enum_delete(DataType *type){
+    free(type);
+}
+
+bool enums_check(DataType *list_one, DataType *list_two, int len_one, int len_two){
+    
+    if (len_one == len_two) {
+        for (int i = 0; i < len_one; i++) {
+            if (list_one[i] != list_two[i]){
+                return false;
+            };
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
