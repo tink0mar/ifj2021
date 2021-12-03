c229f3475a1f493776b43c5c1a78471cec2f577b
DenAd2232 <80587718+DenAd2232@users.noreply.github.com>
2021-12-03 16:30:41 +0100

    Update stack.c

--- stack.c
+++ stack.c
@@ -8,9 +8,9 @@
  */
 
 #include "symtable.h"
-#include <stdlib.h>
 #include "error.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 
 int STACK_SIZE = MAX_STACK;
@@ -54,11 +54,13 @@ void sym_stack_push( SymStack *stack, TreeNode *root ) {
     } else {
         stack->topIndex++;
         stack->array[stack->topIndex] = root;
+
+
     }
 }
-void sym_stack_dispose( SymStack *stack){
+void sym_stack_dispose( SymStack *stack, TreeNode *ptr ){
     while (stack->topIndex != -1){
-        TreeNode *ptr = sym_stack_top(stack);
+        ptr = sym_stack_top(stack);
         bst_dispose(&ptr);
         stack_pop(stack);
     }
