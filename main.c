ac90132c685b1a11e86c74f983b38c5d8dbaebdc
tink0mar <80715800+tink0mar@users.noreply.github.com>
2021-12-04 18:00:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,28 @@
+/**
+ * @file main.c
+ * @brief Main code for ifj compiler
+ * @author Martin Kozák
+ *
+ * Project: IFJ compiler
+ * Date: 24.11.2021
+ */
+
+
 #include <stdio.h>
 #include <stdbool.h>
-#include "scanner.h"
+#include "parser.h"
+#include "error.h"
 
 int main(){
 
-
-    while()
-
+    int error = syntactic_analyzator();
+    
+    if (error == 0){
+        return 0;
+    } else {
+        error_mess(error);
+        return error;
+    }
 
 
 }
